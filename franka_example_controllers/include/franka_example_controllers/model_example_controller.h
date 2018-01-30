375ebdba46d36448fca2f4c7c8505644c3511ece
Florian Walch <florian.walch@franka.de>
2018-01-30 18:14:49 +0100

    Rebase from 'upstream'

--- franka_example_controllers/include/franka_example_controllers/model_example_controller.h
+++ franka_example_controllers/include/franka_example_controllers/model_example_controller.h
@@ -11,22 +11,24 @@
 #include <ros/time.h>
 
 #include <franka_hw/franka_model_interface.h>
+#include <franka_hw/franka_state_interface.h>
 #include <franka_hw/trigger_rate.h>
 
 namespace franka_example_controllers {
 
 class ModelExampleController
-    : public controller_interface::MultiInterfaceController<franka_hw::FrankaModelInterface> {
+    : public controller_interface::MultiInterfaceController<franka_hw::FrankaModelInterface,
+                                                            franka_hw::FrankaStateInterface> {
  public:
-  ModelExampleController();
-  bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& node_handle);
-  void update(const ros::Time&, const ros::Duration&);
+  bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& node_handle) override;
+  void update(const ros::Time&, const ros::Duration&) override;
 
  private:
+  franka_hw::FrankaStateInterface* franka_state_interface_;
+  std::unique_ptr<franka_hw::FrankaStateHandle> franka_state_handle_;
   franka_hw::FrankaModelInterface* model_interface_;
   std::unique_ptr<franka_hw::FrankaModelHandle> model_handle_;
-  franka_hw::TriggerRate rate_trigger_;
-  std::string arm_id_;
+  franka_hw::TriggerRate rate_trigger_{1.0};
 };
 
 }  // namespace franka_example_controllers
