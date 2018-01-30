ab58290633ebf4df2ae072157008d77573f6e916
Florian Walch <florian.walch@franka.de>
2018-01-30 18:15:11 +0100

    Rebase from 'upstream'

--- franka_example_controllers/src/joint_velocity_example_controller.cpp
+++ franka_example_controllers/src/joint_velocity_example_controller.cpp
@@ -9,48 +9,77 @@
 #include <hardware_interface/joint_command_interface.h>
 #include <pluginlib/class_list_macros.h>
 #include <ros/ros.h>
-#include <xmlrpcpp/XmlRpcValue.h>
 
 namespace franka_example_controllers {
 
-JointVelocityExampleController::JointVelocityExampleController()
-    : velocity_joint_interface_(nullptr) {}
-
 bool JointVelocityExampleController::init(hardware_interface::RobotHW* robot_hardware,
                                           ros::NodeHandle& root_node_handle,
-                                          ros::NodeHandle& /*controller_node_handle*/) {
+                                          ros::NodeHandle& /* controller_node_handle */) {
   velocity_joint_interface_ = robot_hardware->get<hardware_interface::VelocityJointInterface>();
   if (velocity_joint_interface_ == nullptr) {
     ROS_ERROR(
         "JointVelocityExampleController: Error getting velocity joint interface from hardware!");
     return false;
   }
-  XmlRpc::XmlRpcValue parameters;
-  if (!root_node_handle.getParam("joint_names", parameters)) {
+  std::vector<std::string> joint_names;
+  if (!root_node_handle.getParam("joint_names", joint_names)) {
     ROS_ERROR("JointVelocityExampleController: Could not parse joint names");
   }
-  if (parameters.size() != 7) {
+  if (joint_names.size() != 7) {
     ROS_ERROR_STREAM("JointVelocityExampleController: Wrong number of joint names, got "
-                     << int(parameters.size()) << " instead of 7 names!");
+                     << joint_names.size() << " instead of 7 names!");
     return false;
   }
   velocity_joint_handles_.resize(7);
-  joint_names_.resize(7);
   for (size_t i = 0; i < 7; ++i) {
-    joint_names_[i] = static_cast<std::string>(parameters[i]);
     try {
-      velocity_joint_handles_[i] = velocity_joint_interface_->getHandle(joint_names_[i]);
+      velocity_joint_handles_[i] = velocity_joint_interface_->getHandle(joint_names[i]);
     } catch (const hardware_interface::HardwareInterfaceException& ex) {
       ROS_ERROR_STREAM(
           "JointVelocityExampleController: Exception getting joint handles: " << ex.what());
       return false;
     }
   }
-  elapsed_time_ = ros::Duration(0.0);
+
+  std::string arm_id;
+  if (!root_node_handle.getParam("arm_id", arm_id)) {
+    ROS_ERROR("JointVelocityExampleController: Could not get parameter arm_id");
+    return false;
+  }
+
+  auto state_interface = robot_hardware->get<franka_hw::FrankaStateInterface>();
+  if (state_interface == nullptr) {
+    ROS_ERROR("JointVelocityExampleController: Could not get state interface from hardware");
+    return false;
+  }
+
+  try {
+    auto state_handle = state_interface->getHandle(arm_id + "_robot");
+
+    std::array<double, 7> q_start{{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
+    for (size_t i = 0; i < q_start.size(); i++) {
+      if (std::abs(state_handle.getRobotState().q_d[i] - q_start[i]) > 0.1) {
+        ROS_ERROR_STREAM(
+            "JointVelocityExampleController: Robot is not in the expected starting position for "
+            "running this example. Run `roslaunch panda_moveit_config move_to_start.launch "
+            "robot_ip:=<robot-ip> load_gripper:=<has-attached-gripper>` first.");
+        return false;
+      }
+    }
+  } catch (const hardware_interface::HardwareInterfaceException& e) {
+    ROS_ERROR_STREAM(
+        "JointVelocityExampleController: Exception getting state handle: " << e.what());
+    return false;
+  }
+
   return true;
 }
 
-void JointVelocityExampleController::update(const ros::Time& /*time*/,
+void JointVelocityExampleController::starting(const ros::Time& /* time */) {
+  elapsed_time_ = ros::Duration(0.0);
+}
+
+void JointVelocityExampleController::update(const ros::Time& /* time */,
                                             const ros::Duration& period) {
   elapsed_time_ += period;
 
